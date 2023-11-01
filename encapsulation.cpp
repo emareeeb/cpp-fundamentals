1b575e4206eb97ea10573ce315a3a1f47ef217c3
Areeb Ahmed <132186043+emareeeb@users.noreply.github.com>
2023-11-01 18:03:35 +0530

    note mod

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,5 +1,7 @@
 /*
-The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must declare class variables/attributes as private (cannot be accessed from outside the class). If you want others to read or modify the value of a private member, you can provide public get and set methods.
+The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. 
+To achieve this, you must declare class variables/attributes as private (cannot be accessed from outside the class). 
+If you want others to read or modify the value of a private member, you can provide public get and set methods.
 */
 
 #include<iostream>
