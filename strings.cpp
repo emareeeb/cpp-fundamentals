d50dde9578b332434400145f83fc6a0efe447fbf
Areeb Ahmed <132186043+emareeeb@users.noreply.github.com>
2023-11-01 18:05:38 +0530

    library note

--- strings.cpp
+++ strings.cpp
@@ -1,7 +1,7 @@
 //The string type is used to store a sequence of characters (text).
 // This is not a built-in type, but it behaves like one in its most basic usage. 
 //String values must be surrounded by double quotes:
-// incld lib : #include<string>
+// include library : #include<string>                                         
 
 
 #include<iostream>
